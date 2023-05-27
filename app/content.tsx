fc91027e3b3cbd20e63b4bfba248b37a703b0085
Nishant Painter <virtual.nishantpainter@gmail.com>
2023-05-27 22:04:56 +0530

    Migrate to mui

---
+++ app/content.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+export default function Content() {
+  return (
+    <Box
+      component={Container}
+      fixed
+      display="flex"
+      alignItems="center"
+      minHeight="100%"
+    >
+      <Box>
+        <Typography variant="h4" gutterBottom>
+          <b>
+            Nishant&nbsp;
+            <Box component="span" textTransform="uppercase">
+              Painter
+            </Box>
+          </b>
+        </Typography>
+        <Typography mb={2}>Computer Programmer&nbsp;💻🕹️☯️</Typography>
+        <Box display="flex" columnGap={1} mb={2}>
+          {[
+            {
+              title: "Github",
+              href: "https://github.com/nishantpainter",
+              alt: "Image for social site github",
+              img: "/img/github.svg",
+            },
+            {
+              title: "Linkedin",
+              href: "https://in.linkedin.com/in/nishant-painter-31388582",
+              alt: "Image for social site linkedin",
+              img: "/img/linkedin.svg",
+            },
+            {
+              title: "Stackoverflow",
+              href: "https://stackoverflow.com/users/13841339/nishant-painter",
+              alt: "Image for social site stackoverflow",
+              img: "/img/so.svg",
+            },
+            {
+              title: "Medium",
+              href: "https://medium.com/@nishantpainter",
+              alt: "Image for social site medium",
+              img: "/img/medium.svg",
+            },
+          ].map(({ href, title, alt, img }) => (
+            <a key={href} href={href} title={title} target="_blank">
+              <Image height={24} width={30} src={img} alt={alt} />
+            </a>
+          ))}
+        </Box>
+        <Button variant="contained" color="secondary">
+          <Box component={Link} color="inherit" href="/publications">
+            Publications
+          </Box>
+        </Button>
+      </Box>
+    </Box>
+  );
+}
--- app/publications/page.tsx
+++ app/publications/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import Image from "next/image";
+import Content from "./content";
 
 export const metadata: Metadata = {
   title: "Publication | Nishant Painter",
@@ -7,85 +7,5 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-  return (
-    <section className="publication">
-      <div className="content">
-        <h1>Publications</h1>
-        <ul>
-          <li>
-            <h3>
-              Machine-Learning-Based Android Malware Detection Techniques—A
-              Comparative Analysis
-            </h3>
-            <Image
-              height={200}
-              width={135}
-              src="/img/springer-information-and-communication-technology-for-sustainable-development.jpg"
-              alt="Information and Communication Technology for Sustainable Development"
-            />
-            <p>
-              Today, Smartphones can handle myriad of programs and applications
-              that perform a wide varieties of functions. In recent years,
-              Android has been a globally anticipated open source operating
-              system for Smartphones. However, rapid advancement of Android is
-              marred with augmenting threats of Android malwares that perform
-              pernicious activities on Smartphones. Malwares exercising
-              different techniques to dodge existing detection methods offer
-              uncommon challenges for their accurate detection. Signature-based
-              detection approach and machine-learning-based detection approach
-              are the broad classifications for existing Android malware
-              detection techniques. Researchers and antimalware companies have
-              identified the inefficiency of signature-based detection approach
-              and shifted to machine-learning-based detection approach to
-              overcome the limitations of signature-based detection approach.
-              This paper disserts existing machine-learning-based Android
-              malware detection techniques and presents parametric comparison of
-              discussed malware detection techniques. Hence, this paper targets
-              to study various machine-learning-based detection techniques and
-              to establish probable future directions.
-            </p>
-            <a
-              className="btn-primary"
-              href="https://link.springer.com/chapter/10.1007/978-981-10-3932-4_19"
-              target="_blank"
-            >
-              See Publication
-            </a>
-          </li>
-
-          <li>
-            <h3>
-              Comparative Analysis of Android Malware Detection Techniques
-            </h3>
-            <Image
-              height={200}
-              width={135}
-              src="/img/springer-proceedings-of-the-international-conference-on-data-engineering-and-communication-technology.jpg"
-              alt="Information and Communication Technology for Sustainable Development"
-            />
-            <p>
-              In recent years, the widespread adoption of smartphones has led to
-              a new age of information exchange. Among smartphones, Android
-              devices have gained huge popularity due to the open architecture
-              of Android and advanced programmable software framework to develop
-              mobile applications. However, the pervasive adoption of Android is
-              coupled with progressively uncontrollable malware threats. This
-              paper gives an insight of existing work in Android malware
-              detection. Additionally, this paper highlights the parametric
-              comparison of existing Android malware detection techniques. Thus,
-              this paper aims to study various Android malware detection
-              techniques and to identify plausible research direction.
-            </p>
-            <a
-              className="btn-primary"
-              href="https://link.springer.com/book/10.1007/978-981-10-1678-3"
-              target="_blank"
-            >
-              See Publication
-            </a>
-          </li>
-        </ul>
-      </div>
-    </section>
-  );
+  return <Content />;
 }
