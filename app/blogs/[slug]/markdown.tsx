9e50a4f152739832e5c774f81834f2b61bf44b53
Nishant Painter <virtual.nishantpainter@gmail.com>
2023-06-01 08:20:31 +0530

    Override markdown typography with material ui

--- app/blogs/[slug]/markdown.tsx
+++ app/blogs/[slug]/markdown.tsx
@@ -1,5 +1,28 @@
 import Markdown from "markdown-to-jsx";
+import Box from "@mui/material/Box";
+import type { MarkdownToJSX } from "markdown-to-jsx";
+
+const typography = ["h1", "h2", "h3", "h4", "h5", "h6", "p", "small"].reduce(
+  (overrides, tag) => ({
+    ...overrides,
+    [tag]: {
+      component: Box,
+      props: {
+        component: tag,
+      },
+    },
+  }),
+  {}
+);
+
+const overrides: MarkdownToJSX.Overrides = {
+  ...typography,
+};
+
+const options: MarkdownToJSX.Options = {
+  overrides,
+};
 
 export function MarkdownToHTML({ markdown }: { markdown: string }) {
-  return <Markdown>{markdown}</Markdown>;
+  return <Markdown options={options}>{markdown}</Markdown>;
 }
