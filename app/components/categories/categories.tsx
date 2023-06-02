b9b479f2fdb1f2a8f689d02b094cb21253659c45
Nishant Painter <virtual.nishantpainter@gmail.com>
2023-06-02 09:38:55 +0530

    Add background color in category
    - Generate random color based on string

--- app/components/categories/categories.tsx
+++ app/components/categories/categories.tsx
@@ -1,18 +1,43 @@
 import Chip from "@mui/material/Chip";
 import Box from "@mui/material/Box";
+import * as MuiColors from "@mui/material/colors";
 
 type CategoriesProps = {
   items: string[];
+  colorful?: boolean;
 };
 
-export function Categories({ items = [] }: CategoriesProps) {
+const colors = Object.values(MuiColors);
+
+function getRandomColor(str: string) {
+  if (!str) return;
+  let hash = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    hash = hash & hash;
+  }
+
+  hash = ((hash % colors.length) + colors.length) % colors.length;
+
+  return (colors[hash] as any)[100];
+}
+
+export function Categories({ items = [], colorful = true }: CategoriesProps) {
+  const getColorfulProps = (str: string) => {
+    if (!colorful) return {};
+    const backgroundColor = getRandomColor(str);
+
+    return { sx: { backgroundColor } };
+  };
+
   return (
     <Box display="flex" flexWrap="wrap" columnGap={1}>
       {items.map((category) => (
-        <Chip key={category} label={category} />
+        <Chip key={category} label={category} {...getColorfulProps(category)} />
       ))}
     </Box>
   );
 }
 
-export default Categories
+export default Categories;
