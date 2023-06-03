38da40ac24ed846106bb639e00f52402ebae7ba2
Nishant Painter <virtual.nishantpainter@gmail.com>
2023-06-03 13:20:18 +0530

    Update markdown overrides for link and code

--- app/blogs/[slug]/markdown.tsx
+++ app/blogs/[slug]/markdown.tsx
@@ -1,6 +1,12 @@
 import Markdown from "markdown-to-jsx";
-import Box from "@mui/material/Box";
 import type { MarkdownToJSX } from "markdown-to-jsx";
+import Box from "@mui/material/Box";
+import MuiLink from "@mui/material/Link";
+import { styled } from "@mui/material/styles";
+
+const Link = styled(MuiLink)({
+  textDecoration: "none",
+});
 
 const typography = ["h1", "h2", "h3", "h4", "h5", "h6", "p", "small"].reduce(
   (overrides, tag) => ({
@@ -15,8 +21,33 @@ const typography = ["h1", "h2", "h3", "h4", "h5", "h6", "p", "small"].reduce(
   {}
 );
 
+const links = {
+  a: {
+    component: Link,
+    props: {
+      color: "secondary",
+    },
+  },
+};
+
+const codes = {
+  pre: {
+    component: Box,
+    props: {
+      p: 1,
+      borderRadius: 1,
+      sx: {
+        backgroundColor: "grey.200",
+      },
+      component: "pre",
+    },
+  },
+};
+
 const overrides: MarkdownToJSX.Overrides = {
   ...typography,
+  ...links,
+  ...codes,
 };
 
 const options: MarkdownToJSX.Options = {
