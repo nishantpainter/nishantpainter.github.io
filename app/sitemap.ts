3353dbdab1c60fc85f7c63e667573842ff512098
Nishant Painter <virtual.nishantpainter@gmail.com>
2023-06-06 20:10:25 +0530

    Include blogs in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,17 @@
 import { MetadataRoute } from "next";
+import fs from "fs";
+import path from "path";
+
+const blogsDir = path.resolve(process.cwd(), "_blogs");
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const blogs = fs.readdirSync(blogsDir).map((file: string) => {
+    return {
+      url: "https://nishantpainter.com/blogs/" + file.replace(".md", ""),
+      lastModified: new Date(),
+    };
+  });
+
   return [
     {
       url: "https://nishantpainter.com",
@@ -10,6 +21,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: "https://nishantpainter.com/blogs",
       lastModified: new Date(),
     },
+    ...blogs,
     {
       url: "https://nishantpainter.com/projects",
       lastModified: new Date(),
