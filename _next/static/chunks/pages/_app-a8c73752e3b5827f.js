5164c71161b835bfbb9f98e76bc82734b782e483
Nishant Painter <virtual.nishantpainter@gmail.com>
2024-03-31 09:57:48 +0530

    Updates

---
+++ _next/static/chunks/pages/_app-a8c73752e3b5827f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{3718:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(162)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[179],function(){return _(3718),_(8874)}),_N_E=n.O()}]);
---
+++ _next/static/wlJpkVNl_VWgXBXGOJOKp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-e4188813017f75de.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
